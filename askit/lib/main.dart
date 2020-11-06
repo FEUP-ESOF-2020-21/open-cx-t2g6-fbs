3fd900626754ea98afd8fc10a4bb4895b3c03511
Pedro Coelho <up201806802@fe.up.pt>
2020-11-06 12:45:36 +0000

    Page for fake login

--- askit/lib/main.dart
+++ askit/lib/main.dart
@@ -3,21 +3,49 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+    return new MaterialApp(
+      title: 'Askit',
+      home: new HomePageWidget(),
+    );
+  }
+}
+
+/// Opens an [AlertDialog] showing what the user typed.
+class HomePageWidget extends StatefulWidget {
+  HomePageWidget({Key key}) : super(key: key);
+  @override
+  _HomePageWidgetState createState() => new _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Welcome to Askit!'),
+        backgroundColor: Colors.black54,
+      ),
+      body: new Center(
+          child: ElevatedButton(
+            child: Text('Log in'),
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Question()),
+              );
+            },
+          )
       ),
     );
   }
---
+++ askit/lib/question.dart
@@ -0,0 +1,72 @@
+
+
+import 'package:askit/main.dart';
+import 'package:flutter/material.dart';
+
+
+class Question extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new ExampleWidget(),
+    );
+  }
+}
+
+/// Opens an [AlertDialog] showing what the user typed.
+class ExampleWidget extends StatefulWidget {
+  ExampleWidget({Key key}) : super(key: key);
+  @override
+  _ExampleWidgetState createState() => new _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Welcome to Askit!'),
+        backgroundColor: Colors.black54,
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new TextField(
+            minLines : 4,
+            maxLines : 6,
+            controller: _controller,
+            decoration: new InputDecoration(
+              border: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2.0)
+              ),
+              hintText: 'Ask a question',
+            ),
+          ),
+          new RaisedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                child: new AlertDialog(
+                  title: new Text('What you typed'),
+                  content: new Text(_controller.text),
+                ),
+              );
+              /*
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyApp()),
+              );
+              */
+            },
+            child: new Text('DONE'),
+          ),
+        ],
+      ),
+    );
+  }
+}
