a7a5a9c9ad4ec71ea33522a24b6c1282250e6cfd
Zé Miguel Maçães <jose.macaes.2000@gmail.com>
2020-12-15 16:02:38 +0000

    Updated homepage design

--- askit/lib/main.dart
+++ askit/lib/main.dart
@@ -24,22 +24,66 @@ class HomePageWidget extends StatefulWidget {
 class _HomePageWidgetState extends State<HomePageWidget> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('Welcome to Askit!'),
-        backgroundColor: Colors.purple[900],
+    return MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/question_mark.jfif"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+        body: new Center(
+          child: new Column(
+          textDirection: TextDirection.ltr,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          verticalDirection: VerticalDirection.down,
+          children: [
+            new Center(
+              child: 
+                Stack(
+                  children: [
+                    Text("ASKIT",
+                      style: TextStyle(
+                        fontSize: 50,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 6
+                          ..color = Colors.purple[900]
+                        ),
+                      ),
+                    Text("ASKIT",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(6.0, 6.0),
+                            blurRadius: 8.0,
+                            color: Colors.purple[900],
+                          ), 
+                        ],
+                        ),
+                      ),
+                  ],
+                )
+              ) ,
+            SizedBox(height: 45),
+            new ElevatedButton(
+              child: Text('Log In', style: new TextStyle(fontSize: 25)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              style: ElevatedButton.styleFrom(primary: Colors.purple[900], minimumSize: Size(120, 70), side: BorderSide(width: 3.0, color: Colors.white), shadowColor: Colors.black)
+            )
+          ],
+        ),
+          ),
+        ),
       ),
-      body: new Center(
-          child: ElevatedButton(
-        child: Text('Log in'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
-          );
-        },
-      )),
     );
   }
 }
