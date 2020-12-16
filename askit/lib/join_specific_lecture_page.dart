a9e8f8f31256466291db0507c310b31c94768a5a
Zé Miguel Maçães <jose.macaes.2000@gmail.com>
2020-12-16 12:01:38 +0000

    Updated join specific lecture design

--- askit/lib/join_specific_lecture_page.dart
+++ askit/lib/join_specific_lecture_page.dart
@@ -22,30 +22,35 @@ class _JoinSpecificLectureState extends State<JoinSpecificLecturePage> {
   Widget build(BuildContext context) {
     final snackBar = SnackBar(content: Text('Joining lecture...'));
     return new Scaffold(
-        appBar: PreferredSize(
-            // Change this argument to customize the height of the app bar
-            preferredSize: Size.fromHeight(50.0),
-            child: AppBar(title: Text('HOME', style: TextStyle(fontSize: 30)))),
+        backgroundColor: new Color.fromARGB(255, 190, 180, 255),
         body: Builder(builder: (BuildContext context) {
           return new Column(
             children: [
-              new Container(
-                  child:
-                      Text('SELECTED LECTURE', style: TextStyle(fontSize: 20)),
-                  padding: EdgeInsets.only(top: 80.0, bottom: 20)),
+               SizedBox(height: 35),
+              _titleText("Selected Lecture", 40),
+               SizedBox(height: 35),
               new ListTile(
                 title: Text(lecture.printIdAndTitle() +
                     "\n\n" +
                     lecture.printTheRest()),
               ),
+              SizedBox(height: 35),
               ElevatedButton(
-                  child: Text('Join'),
+                child: Text('Join', style: new TextStyle(fontSize: 25)),
                   onPressed: () {
                     Scaffold.of(context).showSnackBar(snackBar);
                     associateLectureWithUser().then((result) {
                       Navigator.pop(context);
                     });
-                  }),
+                  },
+                style: 
+                ElevatedButton.styleFrom(
+                  primary: Colors.purple[900],
+                  minimumSize: Size(120, 70),
+                  side: BorderSide(width: 3.0, color: Colors.white),
+                  shadowColor: Colors.black
+                )
+              ),
             ],
           );
         }));
@@ -59,4 +64,36 @@ class _JoinSpecificLectureState extends State<JoinSpecificLecturePage> {
     var encoded = Uri.encodeFull(url);
     await http.get(encoded);
   }
+}
+
+Widget _titleText(String text, double size) {
+  return Center(
+    child: 
+      Stack(
+        children: [
+          Text(text,
+            style: TextStyle(
+              fontSize: size,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 6
+                ..color = Colors.purple[900]
+              ),
+            ),
+          Text(text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: size,
+              shadows: <Shadow>[
+                Shadow(
+                  offset: Offset(6.0, 6.0),
+                  blurRadius: 8.0,
+                  color: Colors.purple[900],
+                ), 
+              ],
+            ),
+          ),
+        ],
+      )
+  );
 }
